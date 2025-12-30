1eb741f76c9dae4e728dc8a7542a3d2ad75ddbba
socorromarques <msocorrosmarques@gmail.com>
2025-12-30 17:43:10 +0000

    fix: actually make clerk auth call async

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -3,7 +3,7 @@ import { auth } from '@clerk/nextjs/server';
 import { prisma } from '@/lib/prisma';
 
 export async function GET() {
-  const { userId } = auth();
+  const { userId } = await auth();
   
   if (!userId) {
     return new NextResponse("Unauthorized", { status: 401 });
@@ -27,7 +27,7 @@ export async function GET() {
 }
 
 export async function POST(req: Request) {
-  const { userId } = auth();
+  const { userId } = await auth();
 
   if (!userId) {
     return new NextResponse("Unauthorized", { status: 401 });
